7727623e64ca7735a18fb114a2a8e6d61fa014f7
Riccardo <riccardo@neutag.com>
2021-11-09 18:09:02 +0100

    fixed subdomain

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ var bodyParser = require("body-parser");
 var app = express();
 var port = 3632;
 var subdomain = "BewerbungZacherMedia";
-var pass = require(`${__dirname}/${subdomain}/pass/pass.json`).pass;
+var pass = require(`${__dirname}/pass/pass.json`).pass;
 
-const behindPass = require(`${__dirname}/${subdomain}/behindPass/loadBehindPass.json`);
+const behindPass = require(`${__dirname}/behindPass/loadBehindPass.json`);
 app.use(bodyParser());
 
 app.use(favicon(path.join(__dirname, subdomain, "favicon.ico")));
-app.use("/main", express.static(`${__dirname}/${subdomain}/main.js`));
-app.use("/static", express.static(`${__dirname}/${subdomain}/static`));
-app.use("/modules", express.static(`${__dirname}/${subdomain}/modules/`));
-app.use("/style.css", express.static(`${__dirname}/${subdomain}/style.css`));
-app.use("/bTxt", express.static(`${__dirname}/${subdomain}/behindPass/buildText.js`));
+app.use(`/${subdomain}/main`, express.static(`${__dirname}/main.js`));
+app.use(`/${subdomain}/static`, express.static(`${__dirname}/static`));
+app.use(`/${subdomain}/modules`, express.static(`${__dirname}/modules/`));
+app.use(`/${subdomain}/style.css`, express.static(`${__dirname}/style.css`));
+app.use(`/${subdomain}/bTxt`, express.static(`${__dirname}/behindPass/buildText.js`));
 
 
 app.get("/passLength", (req, res) => {
   console.log(pass.length); //send pass length to client
   res.send({ l: pass.length });
 });
-app.get("/", (req, res) => res.sendFile(`${__dirname}/${subdomain}/index.html`));
-app.get("/t", (req, res) => res.sendFile(`${__dirname}/${subdomain}/testsite.html`));
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.get("/t", (req, res) => res.sendFile(`${__dirname}/testsite.html`));
 
 app.post("/sendPass", (req, res) => {
   if (req.body.pass == pass) res.json(behindPass);
