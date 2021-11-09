1cf6795696ebd0368864e46f7e12044783964d9b
Riccardo <riccardo@neutag.com>
2021-11-09 18:11:55 +0100

    another subdomain fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var pass = require(`${__dirname}/pass/pass.json`).pass;
 const behindPass = require(`${__dirname}/behindPass/loadBehindPass.json`);
 app.use(bodyParser());
 
-app.use(favicon(path.join(__dirname, subdomain, "favicon.ico")));
+app.use(favicon(path.join(__dirname, "favicon.ico")));
 app.use(`/${subdomain}/main`, express.static(`${__dirname}/main.js`));
 app.use(`/${subdomain}/static`, express.static(`${__dirname}/static`));
 app.use(`/${subdomain}/modules`, express.static(`${__dirname}/modules/`));
@@ -19,14 +19,14 @@ app.use(`/${subdomain}/style.css`, express.static(`${__dirname}/style.css`));
 app.use(`/${subdomain}/bTxt`, express.static(`${__dirname}/behindPass/buildText.js`));
 
 
-app.get("/passLength", (req, res) => {
+app.get(`/${subdomain}/passLength`, (req, res) => {
   console.log(pass.length); //send pass length to client
   res.send({ l: pass.length });
 });
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
-app.get("/t", (req, res) => res.sendFile(`${__dirname}/testsite.html`));
+app.get(`/${subdomain}/t`, (req, res) => res.sendFile(`${__dirname}/testsite.html`));
 
-app.post("/sendPass", (req, res) => {
+app.post(`/${subdomain}/sendPass`, (req, res) => {
   if (req.body.pass == pass) res.json(behindPass);
   if (req.body.pass != pass) res.json({ successfullLoad: false });
 });
